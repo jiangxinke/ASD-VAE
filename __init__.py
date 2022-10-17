7159ff325c6aa7fd8d9b450eb11a9a56d7785c18
ASD-VAE-Anonymous <115765340+ASD-VAE-Anonymous@users.noreply.github.com>
2022-10-17 21:34:45 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .data_utils import NodeClsData, LinkPredData
-from .missing import generate_mask, apply_mask, ASD_apply_mask
-from .gen_structure import prepare_structure
-from .plot_TSNE import plot_TSNE
-from .args import add_args
